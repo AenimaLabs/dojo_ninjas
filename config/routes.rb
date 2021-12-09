ba0f66659c819527dad0c16a00234b5d87935d4a
JorgeAndaur <aenimalabs@gmail.com>
2021-12-08 21:56:47 -0300

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'dojo/index'
+  get 'dojo/show'
+  get 'dojo/new'
+  get 'dojo/edit'
+  get 'dojo/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
