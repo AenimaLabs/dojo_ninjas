409a7b6c8b0f0aa3243be168a4d710153d657790
JorgeAndaur <aenimalabs@gmail.com>
2021-12-12 20:34:46 -0300

    Mostrar acciones Index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   #get 'ninja/new'
   #get 'ninja/edit'
   #get 'ninja/delete'
-  #root 'dojo#index'   #127.0.0.1:3000  => va a mostrar la página INDEX
+  root 'dojo#index'   #127.0.0.1:3000  => va a mostrar la página INDEX, define página por defecto o de inicio
 
   # get 'dojo/index'
   # get 'dojo/show'
